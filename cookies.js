9c0f88bb0f025e8b2ab8d694d506b917559973ad
smj-edison <stephen.mason.jones@gmail.com>
2019-09-02 13:40:45 -0700

    Move cookie management to another file

---
+++ cookies.js
@@ -0,0 +1,41 @@
+function cookiesToCookieString(cookies) {
+    return cookies.map((cookie) => {
+        return cookie.substring(0, cookie.indexOf(";"));
+    }).join("; ");
+};
+
+function getCookieValue(cookies, cookieName) {
+    var cookie = cookies.find(cookie => {
+        return cookie.indexOf(cookieName) === 0;
+    });
+
+    return cookie.substring(cookie.indexOf("=") + 1, cookie.lastIndexOf("; "));
+}
+
+function cookieToKeyValue(cookie) {
+    var cookieKey = cookie.substring(0, cookie.indexOf("="));
+
+    return [cookieKey, cookie.substring(cookieKey.length + 1)];
+}
+
+function mergeCookies(oldCookies, newCookies) {
+    var cookies = {};
+
+    oldCookies.map(cookieToKeyValue).forEach(cookie => {
+        cookies[cookie[0]] = cookie[1];
+    });
+
+    newCookies.map(cookieToKeyValue).forEach(cookie => {
+        cookies[cookie[0]] = cookie[1];
+    });
+
+    var cookiesArray = [];
+
+    for(var i in cookies) {
+        cookiesArray.push(i + "=" + cookies[i]);
+    }
+
+    return cookiesArray;
+}
+
+export {cookiesToCookieString, getCookieValue, cookieToKeyValue, mergeCookies};
