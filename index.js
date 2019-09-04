80ff81eb17ad78b8be7d2842bbe0790d5f8cb451
smj-edison <stephen.mason.jones@gmail.com>
2019-09-04 16:18:16 -0700

    Switch to axios (forgot to commit)

--- index.js
+++ index.js
@@ -1,85 +1,78 @@
 "use strict"
 
-let request = require("request");
+//qs is for generating POST request bodies
+let qs = require("qs");
+//axios is what makes all the requests
+let axios = require("axios");
 
-let jar = request.jar();
-
+//the JSON that is in all program requests
 const PROGRAM_DEFAULT_JSON = require("./constants").PROGRAM_SAVE_JSON_DEFAULT;
 
-function statusCodePromise(resolve, reject) {
-    return (error, response, body) => {
-        console.error(error);
-        console.log(response);
+//some functions I created to work with cookie headers, because axios wasn't doing its job
+let cookieHelper = require("./cookies.js");
 
-        let statusCodeFirstNumber = Math.floor(response.statusCode / 100);
-
-        if (statusCodeFirstNumber == 2) {
-            resolve(body);
-        } else {
-            reject(error);
+//create a configuration for axios requests with all the `cookies` provided
+function genCookieHeader(cookies) {
+    return {
+        headers: {
+            Cookie: cookieHelper.cookiesToCookieString(cookies)
         }
     }
 }
 
-function getRequest(uri, settings={}) {
-    return new Promise((resolve, reject) => {
-        request(uri, settings, statusCodePromise(resolve, reject))
+//load khanacademy.org and return a list of all the cookies
+function getSessionCookies() {
+    return axios.get("https://khanacademy.org").then((result) => {
+        return result.headers["set-cookie"];
+    }, {withCredentials: true});
+}
+
+//logs in a user, based on their username, password, and the cookies from `getSessionCookies()`
+async function login(username, password, cookies) {
+    return axios.post("https://www.khanacademy.org/login", qs.stringify({
+        "identifier": username,
+        "password": password,
+        "fkey": cookieHelper.getCookieValue(cookies, "fkey"),
+        "continue": "/"
+    }), { withCredentials: true }).then((result) => {
+        return result.headers["set-cookie"];
     });
 }
 
-function getSessionCookies(cookies) {
-    return getRequest("https://khanacademy.org", {
-        jar: cookies
-    });
-}
-
-async function login(cookies, username, password) {
-    return new Promise((resolve, reject) => {
-        request.post("https://www.khanacademy.org/login", {
-            jar: cookies
-        }, statusCodePromise(resolve, reject)).form({
-            "identifier": username,
-            "password": password,
-            "fkey": cookies._jar.store.idx["www.khanacademy.org"]["/"].fkey.value,
-            "continue": "/"
-        });
-    });
-}
-
-//returns a promise for a GET request to the URI passed in
-
-
-function getProgramJSON(id) {
-    return getRequest("https://www.khanacademy.org/api/internal/scratchpads/" + id);
+//returns the parsed program JSON from the API
+async function getProgramJSON(id) {
+    return axios.get("https://www.khanacademy.org/api/internal/scratchpads/" + id).then(response => { return response.data; });
 }
 
 // 190812-1645-93fee248ac6f_1565658007119
 // 190812-1645-93fee248ac6f_1565658171688
 // 190812-1645-93fee248ac6f_1565658565643
 
-// ??????-????-????????????_(new Date()).getTime()
+// ?cache-numb-er??????????_(new Date()).getTime()
 
 function getQueryTime() {
-    var timeModifier = (new Date()).toTimeString();
-    timeModifier = timeModifier.substring(timeModifier.indexOf("GMT") + 3, timeModifier.indexOf("GMT") + 8);
-    timeModifier = timeModifier.substring(0, 3) + ":" + timeModifier.substring(3);
-    return (new Date((new Date()) - (new Date(parseInt(timeModifier) * 60 * 60 * 1000)))).toISOString().substring(0, 19) + timeModifier;
+    var now = new Date();
+
+    var timeZone = now.toTimeString();
+    timeZone = timeZone.substring(timeZone.indexOf("GMT") + 3, timeZone.indexOf("GMT") + 8);
+    timeZone = timeZone.substring(0, 3) + ":" + timeZone.substring(3);
+
+    //TODO: this needs to be cleaned up, and also stablized
+    return (new Date(now.getTime() - now.getTimezoneOffset() * 60000)).toISOString().substring(0, 19) + timeZone;
 }
 
 async function updateProgram(cookies, programId, programType, code, settings={}) {
-    var programJson = JSON.parse(await getProgramJSON(programId)); //get the program's JSON, is this necessary?
+    var programJson = await getProgramJSON(programId); //get the program's JSON, is this necessary?
 
     var jsonToSend = {
         ...PROGRAM_DEFAULT_JSON,
         ...programJson,
         "relativeUrl": "/computer-programming/_/" + programId,
         "id": parseInt(programId),
-        /*"kaid": kaid, //provided when loading programJson */
         "date": (new Date()).toISOString().substring(0, 19) + "Z",
         "revision": {
             ...PROGRAM_DEFAULT_JSON.revision,
             ...programJson.revision,
-            "created": (new Date()).toISOString().substring(0, 19) + "Z",
             "code": code
         },
         "trustedRevision": {
@@ -87,21 +80,48 @@ async function updateProgram(cookies, programId, programType, code, settings={})
         },
         ...settings
     };
-
-    return new Promise((resolve, reject) => {
-        request(`https://www.khanacademy.org/api/internal/scratchpads/${programId}?client_dt=${getQueryTime()}&lang=en`, {
-            method: "PUT",
-            json: jsonToSend,
-            jar: cookies
-        }, statusCodePromise(resolve, reject));
-    });
+    console.log("logged in!");
+    return axios.put(`https://www.khanacademy.org/api/internal/scratchpads/${programId}?client_dt=${getQueryTime()}&lang=en`, jsonToSend, {
+            headers: {
+                Cookie: cookieHelper.cookiesToCookieString(cookies)
+            }});
 }
+
+async function commentAtRoot(commentURL, text, cookies) {
+    var commentJSON = {
+        "fromVideoAuthor": false,
+        "shownLowQualityNotice": false,
+        "text": text,
+        "topic_slug": "computer-programming"
+    }
+
+    axios.post(commentURL + `?casing=camel&lang=en&_=190828-1155-f259d8dcd107_${Date.now()}`, commentJSON, {
+        headers: {
+            Cookie: cookieHelper.cookiesToCookieString(cookies)
+        }
+    });
+};
 
 let username = process.env.USERNAME;
 let password = process.env.PASSWORD;
 
-(async function() {
-    await getSessionCookies(jar);
-    await login(jar, username, password);
-    await updateProgram(jar, "0000000000000000", "pjs", "//updated from node!");
+let discard = (async function() {
+    let sessionCookies = await getSessionCookies();
+    console.log("got session cookies!");
+
+    let loginCookies = await login(username, password, sessionCookies);
+
+    let cookies = cookieHelper.mergeCookies(sessionCookies, loginCookies);
+
+    //check that you are logged in
+    console.assert((await axios.get("https://khanacademy.org", genCookieHeader(cookies))).data.indexOf(username) >= 0,
+        {errorMsg: "Not logged in correctly! When opening up khanacademy.org, it shows default page"});
+
+    console.log("logged in!");
+
+    updateProgram(cookies, process.env.TEST_PROGRAM, "pjs", "//updated from node!").then(results => {
+        console.log("Got results!", results);
+    }).catch(err => {
+        console.log("Error in changing program: ", err);
+    });
 })();
