44c178faf1ee9bc9b77604bc84fd24849b656400
smj-edison <stephen.mason.jones@gmail.com>
2020-03-23 20:04:34 -0700

    Don't use selenium after all

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 //qs is for generating POST request bodies
 let qs = require("qs");
+//axios is what makes all the requests
 let axios = require("axios");
-let getSessionCookies = require("./get_session_cookies");
 
 //the JSON that is in all program requests
 const PROGRAM_DEFAULT_JSON = require("./constants").PROGRAM_SAVE_JSON_DEFAULT;
@@ -11,19 +11,32 @@ const PROGRAM_DEFAULT_JSON = require("./constants").PROGRAM_SAVE_JSON_DEFAULT;
 //some functions I created to work with cookie headers, because axios wasn't doing its job
 let cookieHelper = require("./cookies.js");
 
-function getCookieStringFromObject(cookies) {
-    return cookies.reduce((cookieString, cookie) => {
-        return cookieString + (cookieString ? "; " : "") + cookie.name + "=" + cookie.value;
-    }, "");
-}
-
 //create a configuration for axios requests with all the `cookies` provided
 function genCookieHeader(cookies) {
     return {
         headers: {
-            Cookie: getCookieStringFromObject(cookies)
+            Cookie: cookieHelper.cookiesToCookieString(cookies)
         }
     }
+}
+
+//load khanacademy.org and return a list of all the cookies
+function getSessionCookies() {
+    return axios.get("https://khanacademy.org").then((result) => {
+        return result.headers["set-cookie"];
+    }, {withCredentials: true});
+}
+
+//logs in a user, based on their username, password, and the cookies from `getSessionCookies()`
+async function login(username, password, cookies) {
+    return axios.post("https://www.khanacademy.org/login", qs.stringify({
+        "identifier": username,
+        "password": password,
+        "fkey": cookieHelper.getCookieValue(cookies, "fkey"),
+        "continue": "/"
+    }), { withCredentials: true }).then((result) => {
+        return result.headers["set-cookie"];
+    });
 }
 
 //returns the parsed program JSON from the API
@@ -66,7 +79,8 @@ async function updateProgram(cookies, programId, programType, code, settings={})
     
     return axios.put(`https://www.khanacademy.org/api/internal/scratchpads/${programId}?client_dt=${getQueryTime()}&lang=en`, jsonToSend, {
             headers: {
-                Cookie: cookieHelper.cookiesToCookieString(cookies)
+                Cookie: cookieHelper.cookiesToCookieString(cookies),
+                "X-KA-FKey": cookieHelper.getCookieValue(cookies, "fkey")
             }});
 }
 
@@ -78,7 +92,12 @@ async function commentAtRoot(commentURL, text, cookies) {
         "topic_slug": "computer-programming"
     }
 
-    axios.post(commentURL + `?casing=camel&lang=en&_=190828-1155-f259d8dcd107_${Date.now()}`, commentJSON, genCookieHeader(cookies));
+    axios.post(commentURL + `?casing=camel&lang=en&_=190828-1155-f259d8dcd107_${Date.now()}`, commentJSON, {
+        headers: {
+            Cookie: cookieHelper.cookiesToCookieString(cookies),
+            "X-KA-FKey": cookieHelper.getCookieValue(cookies, "fkey")
+        }
+    });
 };
 
 let username = process.env.USERNAME;
@@ -86,10 +105,15 @@ let password = process.env.PASSWORD;
 
 //capture results so that they aren't printed to the console
 let _ = (async function() {
-    let loginCookies = await getSessionCookies(username, password);
+    let sessionCookies = await getSessionCookies();
+    console.log("got session cookies!");
+
+    let loginCookies = await login(username, password, sessionCookies);
+
+    let cookies = cookieHelper.mergeCookies(sessionCookies, loginCookies);
 
     //check that you are logged in
-    console.assert((await axios.get("https://khanacademy.org/profile", genCookieHeader(cookies))).data.indexOf(username) >= 0,
+    console.assert((await axios.get("https://khanacademy.org", genCookieHeader(cookies))).data.indexOf(username) >= 0,
         {errorMsg: "Not logged in correctly! When opening up khanacademy.org, it shows default page"});
 
     console.log("logged in!");
