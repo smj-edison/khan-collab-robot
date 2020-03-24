4159e4a1625893f5b8e6b41f07906f52562af16b
smj-edison <stephen.mason.jones@gmail.com>
2020-03-23 20:31:39 -0700

    Move program code into programs.js

--- index.js
+++ index.js
@@ -3,52 +3,7 @@
 const axios = require("axios");
 const cookieHelper = require("./cookies.js");
 const {getSessionCookies, login} = require("./session.js");
-const PROGRAM_DEFAULT_JSON = require("./constants").PROGRAM_SAVE_JSON_DEFAULT;
-
-//returns the parsed program JSON from the API
-async function getProgramJSON(id) {
-    return axios.get("https://www.khanacademy.org/api/internal/scratchpads/" + id).then(response => { return response.data; });
-}
-
-// ?cache-numb-er??????????_(new Date()).getTime()
-
-function getQueryTime() {
-    var now = new Date();
-
-    var timeZone = now.toTimeString();
-    timeZone = timeZone.substring(timeZone.indexOf("GMT") + 3, timeZone.indexOf("GMT") + 8);
-    timeZone = timeZone.substring(0, 3) + ":" + timeZone.substring(3);
-
-    //TODO: this needs to be cleaned up, and also stablized
-    return (new Date(now.getTime() - now.getTimezoneOffset() * 60000)).toISOString().substring(0, 19) + timeZone;
-}
-
-async function updateProgram(cookies, programId, programType, code, settings={}) {
-    var programJson = await getProgramJSON(programId); //get the program's JSON, is this necessary?
-
-    var jsonToSend = {
-        ...PROGRAM_DEFAULT_JSON,
-        ...programJson,
-        "relativeUrl": "/computer-programming/_/" + programId,
-        "id": parseInt(programId),
-        "date": (new Date()).toISOString().substring(0, 19) + "Z",
-        "revision": {
-            ...PROGRAM_DEFAULT_JSON.revision,
-            ...programJson.revision,
-            "code": code
-        },
-        "trustedRevision": {
-            "created": (new Date()).toISOString()
-        },
-        ...settings
-    };
-    
-    return axios.put(`https://www.khanacademy.org/api/internal/scratchpads/${programId}?client_dt=${getQueryTime()}&lang=en`, jsonToSend, {
-            headers: {
-                Cookie: cookieHelper.cookiesToCookieString(cookies),
-                "X-KA-FKey": cookieHelper.getCookieValue(cookies, "fkey")
-            }});
-}
+const {updateProgram} = require("./programs");
 
 async function commentAtRoot(commentURL, text, cookies) {
     var commentJSON = {
---
+++ programs.js
@@ -0,0 +1,65 @@
+const axios = require("axios");
+const {makePostRequest} = require("./session");
+const PROGRAM_DEFAULT_JSON = require("./constants").PROGRAM_SAVE_JSON_DEFAULT;
+
+/**
+ * A helper function that returns the current time formatted for KA's servers
+ */
+function getQueryTime() {
+    var now = new Date();
+
+    var timeZone = now.toTimeString();
+    timeZone = timeZone.substring(timeZone.indexOf("GMT") + 3, timeZone.indexOf("GMT") + 8);
+    timeZone = timeZone.substring(0, 3) + ":" + timeZone.substring(3);
+
+    //TODO: this needs to be cleaned up, and also stablized
+    return (new Date(now.getTime() - now.getTimezoneOffset() * 60000)).toISOString().substring(0, 19) + timeZone;
+}
+
+/**
+ * Returns the parsed program JSON for a program, given the program's ID
+ * @param {*} id The program's ID
+ */
+async function getProgramJSON(id) {
+    return axios.get("https://www.khanacademy.org/api/internal/scratchpads/" + id)
+                .then(response => {
+                    return response.data;
+                });
+}
+
+/**
+ * Updates an existing program based on the parameters
+ * @param {Array} cookies An array of set-cookie response headers from axios
+ * @param {*} programId The program's ID being updated
+ * @param {*} code A string of with the code
+ * @param {*} settings Settings to override the JSON request
+ */
+async function updateProgram(cookies, programId, code, settings={}) {
+    var programJson = await getProgramJSON(programId); //get the program's JSON, is this necessary?
+
+    var jsonToSend = {
+        ...PROGRAM_DEFAULT_JSON,
+        ...programJson,
+        "relativeUrl": "/computer-programming/_/" + programId,
+        "id": parseInt(programId),
+        "date": (new Date()).toISOString().substring(0, 19) + "Z",
+        "revision": {
+            ...PROGRAM_DEFAULT_JSON.revision,
+            ...programJson.revision,
+            "code": code
+        },
+        "trustedRevision": {
+            "created": (new Date()).toISOString()
+        },
+        ...settings
+    };
+
+    let url = `https://www.khanacademy.org/api/internal/scratchpads/${programId}?client_dt=${getQueryTime()}&lang=en`;
+    
+    return makePostRequest(url, jsonToSend, cookies);
+}
+
+module.exports = {
+    getProgramJSON,
+    updateProgram
+};
