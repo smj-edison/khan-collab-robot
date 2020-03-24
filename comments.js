9d766734e027abbf96b210aee971a2d1a85dc08b
smj-edison <stephen.mason.jones@gmail.com>
2020-03-24 14:11:31 -0700

    Add argument to comment on program id

--- comments.js
+++ comments.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const {makePostRequest} = require("./session");
 
-async function commentAtRoot(commentURL, text, cookies) {
+async function commentAtRoot(programId, text, cookies) {
     var commentJSON = {
         "fromVideoAuthor": false,
         "shownLowQualityNotice": false,
@@ -9,7 +9,8 @@ async function commentAtRoot(commentURL, text, cookies) {
         "topic_slug": "computer-programming"
     }
 
-    makePostRequest(`${commentURL}?casing=camel&lang=en&_=190828-1155-f259d8dcd107_${Date.now()}`, commentJSON, cookies);
+    let url = `https://www.khanacademy.org/api/internal/discussions/scratchpad/${programId}/comments?casing=camel&lang=en&_=190828-1155-f259d8dcd107_${Date.now()}`;
+    makePostRequest(url, commentJSON, cookies);
 };
 
 module.exports = {
