6992279bcf0c28ae99e16f74c8d60b90a6109a97
smj-edison <stephen.mason.jones@gmail.com>
2020-03-23 20:12:45 -0700

    Add docs

--- session.js
+++ session.js
@@ -1,14 +1,19 @@
 const axios = require("axios");
 const qs = require("qs");
 
-//load khanacademy.org and return a list of all the cookies
+/**
+ * Load khanacademy.org and return a list of all the cookies
+**/
 function getSessionCookies() {
     return axios.get("https://khanacademy.org").then((result) => {
         return result.headers["set-cookie"];
     }, {withCredentials: true});
 }
 
-//logs in a user, based on their username, password, and the cookies from `getSessionCookies()`
+/**
+ * logs in a user, based on their username, password, and the cookies from `getSessionCookies()`
+ * @returns an array of set-cookie headers from the server
+**/
 async function login(username, password, cookies) {
     return axios.post("https://www.khanacademy.org/login", qs.stringify({
         "identifier": username,
