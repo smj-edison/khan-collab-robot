ae1970581d1c54fca84eea7c3665a796520ee9b4
smj-edison <stephen.mason.jones@gmail.com>
2020-03-24 10:49:05 -0700

    Add makePutRequest

--- session.js
+++ session.js
@@ -39,6 +39,26 @@ async function makePostRequest(url, body, cookies) {
 }
 
 /**
+ * Make a PUT request with the proper authentication on Khan Academy
+ * 
+ * @param {string} url The url on Khan Academy to make the PUT request
+ * @param {string|object} body The body of the PUT request
+ * @param {Array} cookies A list of cookies returned from a axios's request (set-cookie header)
+ */
+async function makePutRequest(url, body, cookies) {
+    if(typeof body === "object") {
+        body = JSON.stringify(body);
+    }
+
+    return axios.put(url, body, {
+        "headers": {
+            "Cookie": cookiesToCookieString(cookies),
+            "X-KA-FKey": getCookieValue(cookies, "fkey")
+        }
+    });
+}
+
+/**
  * Load khanacademy.org and return a list of all the cookies
 **/
 function getSessionCookies() {
@@ -73,6 +93,7 @@ async function login(username, password) {
 module.exports = {
     makeGetRequest,
     makePostRequest,
+    makePutRequest,
     getSessionCookies,
     login
 };
