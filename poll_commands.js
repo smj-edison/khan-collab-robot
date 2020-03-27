5004641f0f5daf0ad160a316a97ff4fc7bced9d3
smj-edison <stephen.mason.jones@gmail.com>
2020-03-27 15:24:11 -0700

    Add function to check for new commands

---
+++ poll_commands.js
@@ -0,0 +1,35 @@
+const {getCommentsOnComment, commentOnComment} = require("./comments");
+const {runCommand} = require("./command_router");
+
+async function pollCommands(commentId, lastComment, cookies) {
+    let allComments = await getCommentsOnComment(commentId);
+
+    let lastCommandExecutedIndex = allComments.findIndex(comment => {
+        return comment.key === lastComment;
+    });
+
+    // delete all commands already executed
+    if(lastCommandExecutedIndex !== -1) {
+        allComments.splice(0, lastCommandExecutedIndex + 1);
+    }
+
+    // get rid of comments by the bot
+    allComments = allComments.filter(comment => {
+        return comment.authorKaid != process.env.BOT_KAID;
+    });
+
+    for(let i = 0; i < allComments.length; i++) {
+        let text = allComments[i].content;
+        let kaid = allComments[i].authorKaid;
+        
+        runCommand(text, kaid).then((function(response) {
+            commentOnComment(this.commentId, response, cookies);
+        }).bind({
+            commentId: commentId
+        }));
+    }
+}
+
+module.exports = {
+    pollCommands
+};
