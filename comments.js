6193c4c26d78a593c4e3264e7b3e7596c3abbab5
smj-edison <stephen.mason.jones@gmail.com>
2020-03-27 15:23:36 -0700

    Create function for commenting on a function

--- comments.js
+++ comments.js
@@ -11,7 +11,17 @@ async function commentAtRoot(programId, text, cookies) {
 
     let url = `https://www.khanacademy.org/api/internal/discussions/scratchpad/${programId}/comments?casing=camel&lang=en&_=190828-1155-f259d8dcd107_${Date.now()}`;
     makePostRequest(url, commentJSON, cookies);
-};
+}
+
+async function commentOnComment(commentId, text, cookies) {
+    var commentJSON = {
+        "text": text,
+        "topic_slug": "computer-programming"
+    }
+
+    let url = `https://www.khanacademy.org/api/internal/discussions/${commentId}/replies?casing=camel&lang=en`;
+    makePostRequest(url, commentJSON, cookies);
+}
 
 async function getProgramComments(programId) {
     let url = `https://www.khanacademy.org/api/internal/discussions/scratchpad/${programId}/comments?casing=camel&limit=1000000&page=0&sort=1&lang=en`;
@@ -30,5 +40,8 @@ async function getCommentsOnComment(commentId) {
 }
 
 module.exports = {
-    commentAtRoot
+    commentAtRoot,
+    commentOnComment,
+    getProgramComments,
+    getCommentsOnComment
 };
