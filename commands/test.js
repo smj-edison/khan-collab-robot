f00e13f51e9f1e0695769e5e8db94f69dd2794e4
smj-edison <stephen.mason.jones@gmail.com>
2020-04-03 07:30:10 -0700

    Log test command output to console

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,6 @@
-function test(args) {
-    return "Hello, ${args[0]}";
+function test(args, kaid) {
+    console.log(`Hello, ${args[0]}`);
+    return `Hello, ${args[0]}`;
 }
 
 module.exports = test;
