af3f7addbc1eb632da76a7d565fec3c0761ec16d
smj-edison <stephen.mason.jones@gmail.com>
2020-04-15 14:31:58 -0700

    Add function to parse program headers

---
+++ program_header.js
@@ -0,0 +1,41 @@
+const {PROGRAM_HEADER_END_STRING} = require("./constants")
+
+function parseProgramHeaders(code) {
+    const endOfHeaders = code.indexOf(PROGRAM_HEADER_END_STRING);
+    let headerString;
+    
+    // if there's the end header string, delete everything after it
+    if(endOfHeaders > -1) {
+        headerString = code.substring(0, endOfHeaders).trim();
+    } else {
+        // just consider everything as a header
+        headerString = code.trim();
+    }
+
+    let headers = {};
+
+    // split the headers by each line
+    headerString.split("\n").forEach(headerRow => {
+        const slashslashIndex = headerRow.indexOf("//");
+        
+        // if the line has a "//"
+        if(slashslashIndex > -1) {
+            // split the line by a ":"
+            const keyValueString = headerRow.substring(slashslashIndex + 2);
+            const keyValue = keyValueString.split(":");
+
+            const key = keyValue[0].trim();
+            const value = keyValue[1].trim();
+
+            headers[key] = value;
+        }
+
+        // else, disregard line
+    });
+
+    return headers;
+}
+
+module.exports = {
+    parseProgramHeaders
+};
