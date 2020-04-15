bb768320ac489e15df08a5817d17a1cfdbc91ba2
smj-edison <stephen.mason.jones@gmail.com>
2020-04-15 15:36:26 -0700

    Add function to get kaid from username

---
+++ profile.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+const {cookiesToCookieString} = require("./cookies");
+const {GET_FULL_USER_PROFILE_QUERY} = require("./constants");
+
+async function getKaidFromUsername(username, cookies) {
+    let body = {
+        "operationName": "getFullUserProfile",
+        "variables":{"username": "username"},
+        "query": GET_FULL_USER_PROFILE_QUERY
+    };
+
+    let url = "https://www.khanacademy.org/api/internal/graphql/getFullUserProfile?lang=en";
+
+    return axios.post(url, body, {
+        "headers": {
+            "Cookie": cookiesToCookieString(cookies)
+        }
+    }).then(result => {
+        return result.data.data.user.kaid;
+    });
+}
+
+module.exports = {
+    getKaidFromUsername
+};
