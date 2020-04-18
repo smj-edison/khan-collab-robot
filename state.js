c04dd1c4a23e5e9f6bda46371ca17949ab4bb363
smj-edison <stephen.mason.jones@gmail.com>
2020-04-18 15:55:21 -0700

    Add function to set the new state

--- state.js
+++ state.js
@@ -3,6 +3,7 @@ const {STATE_FILE_LOCATION} = require("./constants.js");
 
 /**
  * getState returns the current value of the state json file.
+ * @returns {Object} The parsed JSON from the state file
  */
 async function getState() {
     return new Promise(resolve => {
@@ -12,4 +13,12 @@ async function getState() {
             resolve({});
         });
     });
+}
+
+/**
+ * setState writes the new state to the state file
+ * @param {Object} json The JSON to write to the file
+ */
+async function setState(json) {
+    return fsp.writeFile(STATE_FILE_LOCATION, JSON.stringify(json));
 }
