c317bebba8e0aff2b8023af3f5c2e0e2cb87d27f
smj-edison <stephen.mason.jones@gmail.com>
2020-04-18 17:33:08 -0700

    Create a companion history program

--- commands/createprogram.js
+++ commands/createprogram.js
@@ -1,7 +1,22 @@
-const {newProgram} = require("../programs");
+const {newProgram, changeProgramHeaders} = require("../programs");
+const {generateProgramHeaders} = require("../program_header");
 
 async function createprogram(args, kaid, cookies) {
-    const programResult = await newProgram(cookies, `//author: ${kaid}`);
+    // create the program
+    const programResult = await newProgram(cookies, generateProgramHeaders({
+        author: kaid
+    }));
+
+    // as well as a history file
+    const historyProgramResult = await newProgram(cookies, "{}", {
+        title: `${programResult.data.id} history`
+    });
+
+    // set the program's historyprogramid to the history program
+    await changeProgramHeaders(cookies, programResult.data.id, headers => {
+        headers.historyprogramid = historyProgramResult.data.id;
+    });
+
     return `Your program is here: https://www.khanacademy.org${programResult.data.relativeUrl}`;
 }
 
