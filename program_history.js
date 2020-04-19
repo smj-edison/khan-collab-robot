f3d697385efc0da446783eb6a3e8894fd4a0af3b
smj-edison <stephen.mason.jones@gmail.com>
2020-04-18 18:27:59 -0700

    Add program history management functions

---
+++ program_history.js
@@ -0,0 +1,16 @@
+const {getProgramJSON, updateProgram} = require("./programs")
+
+async function loadProgramHistory(programId) {
+    const programJson = await getProgramJSON(programId);
+
+    return JSON.parse(programJson.revision.code);
+}
+
+async function updateProgramHistory(cookies, programId, json) {
+    await updateProgram(cookies, programId, JSON.stringify(json));
+}
+
+module.exports = {
+    loadProgramHistory,
+    updateProgramHistory
+};
