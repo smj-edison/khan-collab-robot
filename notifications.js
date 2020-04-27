501d62d06e2daa71766a01ec6bac95a49ca44a84
smj-edison <stephen.mason.jones@gmail.com>
2020-04-27 16:47:56 -0700

    Add function to return brand new notifications.

--- notifications.js
+++ notifications.js
@@ -1,6 +1,6 @@
 const {makeGetRequest} = require("./session");
 
-async function getNotificationsRequest(cursor) {
+async function getNotificationsRequest(cookies, cursor) {
     let cursorString = cursor ? `&cursor=${cursor}` : "";
     let url = `https://www.khanacademy.org/api/internal/user/notifications/readable?casing=camel${cursorString}&_=200324-0901-e040f1fb5249_${Date.now()}`;
 
@@ -8,6 +8,30 @@ async function getNotificationsRequest(cursor) {
                 .then(response => {
                     return response.data;
                 });
+}
+
+async function getBrandNewNotifications(cookies) {
+    let currentCursor;
+    let notifications = [];
+
+    do {
+        const response = await getNotificationsRequest(cookies);
+        const cursor = response.cursor;
+        let notifs = response.notifications;
+
+        // check if there are new notifications
+        if(notifs[notifs.length - 1].brandNew) {
+            currentCursor = cursor;
+        } else {
+            currentCursor = null;
+        }
+
+        notifications.push(...notifs.filter(notif => {
+            return notif.brandNew;
+        }));
+    } while(currentCursor);
+
+    return notifications;
 }
 
 function parseNotificationJSON(json) {
