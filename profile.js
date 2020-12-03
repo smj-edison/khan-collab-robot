f5fd8836d6185afbd442c638b8c502f19552bc12
smj-edison <stephen.mason.jones@gmail.com>
2020-12-02 18:36:47 -0800

    Fix contributor bug

--- profile.js
+++ profile.js
@@ -5,7 +5,7 @@ const {GET_FULL_USER_PROFILE_QUERY} = require("./constants");
 async function getKaidFromUsername(username) {
     let body = {
         "operationName": "getFullUserProfile",
-        "variables":{"username": "username"},
+        "variables": {"username": username},
         "query": GET_FULL_USER_PROFILE_QUERY
     };
 
