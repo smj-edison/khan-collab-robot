93235234c731d37beb96d842c881a844e068bbfe
smj-edison <stephen.mason.jones@gmail.com>
2020-12-04 17:42:46 -0800

    Add function to delete long discussions

---
+++ discussion_culling.js
@@ -0,0 +1,7 @@
+const {MAX_DISCUSSION_LENGTH} = require("./constants.js");
+
+async function cullLongDiscussions(discussionLength, discussionId) {
+    if(discussionLength > MAX_DISCUSSION_LENGTH) {
+
+    }
+}
