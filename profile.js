683a0f96ec38eb11735df2d8e5f208dae5bb61b2
smj-edison <stephen.mason.jones@gmail.com>
2020-12-07 17:56:56 -0800

    Fix getKaidFromUsername

--- profile.js
+++ profile.js
@@ -1,8 +1,10 @@
 const axios = require("axios");
 const {cookiesToCookieString} = require("./cookies");
+const {makePostRequest} = require("./session");
+const {CommandError} = require("./command_error");
 const {GET_FULL_USER_PROFILE_QUERY} = require("./constants");
 
-async function getKaidFromUsername(username) {
+async function getKaidFromUsername(cookies, username) {
     let body = {
         "operationName": "getFullUserProfile",
         "variables": {"username": username},
@@ -11,7 +13,11 @@ async function getKaidFromUsername(username) {
 
     let url = "https://www.khanacademy.org/api/internal/graphql/getFullUserProfile?lang=en";
 
-    return axios.post(url, body).then(result => {
+    return makePostRequest(url, body, cookies).then(result => {
+        if(Object.is(result.data.data.user, null)) {
+            throw new CommandError(`The username "${username}" does not exist. Make sure that you have the user's username and not their display name.`);
+        }
+
         return result.data.data.user.kaid;
     });
 }
