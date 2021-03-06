288fb5af7300fff66be97398eea7a6835b84c68c
smj-edison <stephen.mason.jones@gmail.com>
2021-03-05 22:23:50 -0800

    Code cleanup and refactoring

--- src/notifications/notifications.js
+++ src/notifications/notifications.js
@@ -1,22 +1,21 @@
-const {makeGetRequest} = require("./session");
-const axios = require("axios");
-const {cookiesToCookieString, getCookieValue} = require("./cookies");
+const {getAllBrandNewNotifications, clearBrandNewNotifications} = require("ka-api").notifications;
+const {getNotificationCommentDetails} = require("ka-api").notifications;
 const {markDiscussionOld} = require("../cleanup/discussion_culling.js");
 
-async function parseNotificationJSON(json) {
-    var notificationType = json.class_[json.class_.length - 1];
+async function parseNotificationJSON(notifJson) {
+    const notificationType = notifJson.class_[notifJson.class_.length - 1];
 
-    // ignore direct comments (as they can't be cascade deleted)
+    // ignore direct comment notifications (as they can't be cascade deleted)
     if(notificationType === "ResponseFeedbackNotification") {
-        let {post, posts} = await getNotificationPost(json);
+        let {post, posts} = await getNotificationCommentDetails(notifJson);
 
-        const programId = json.url.match(/\d+(?=\?)/)[0];
+        const programId = notifJson.url.match(/\d+(?=\?)/)[0];
 
         return {
             type: "response-feedback",
-            parentCommentId: json.feedbackHierarchy[1],
-            commentId: json.feedbackHierarchy[0],
-            value: json.content,
+            parentCommentId: notifJson.feedbackHierarchy[1],
+            commentId: notifJson.feedbackHierarchy[0],
+            value: notifJson.content,
             kaid: post.authorKaid,
             programId: programId,
             postsInDiscussion: posts.length
@@ -27,23 +26,22 @@ async function parseNotificationJSON(json) {
 }
 
 async function getAndParseNewNotifications(cookies) {
-    const notifications = await getBrandNewNotifications(cookies);
-    const clearNotifPromise = clearNewNotifications(cookies);
+    const notifications = await getAllBrandNewNotifications(cookies);
+    const clearNotifPromise = clearBrandNewNotifications(cookies);
 
-    // no need to wait for the response for calculation this
+    // parsing has to make get requests to read the notifications, so the map is promisified
     const parsedNotifs = (await Promise.all(notifications.map(parseNotificationJSON))).filter(notif => notif !== null);
 
     await Promise.all(parsedNotifs.map(async notif => {
-        // clean up long discussions
-        await markDiscussionOld(cookies, notif.postsInDiscussion, notif.programId, notif.parentCommentId);
+        // clean up long discussions TODO: move to a more transparent place
+        await checkAndMarkDiscussionOld(cookies, notif.postsInDiscussion, notif.programId, notif.parentCommentId);
     }));
 
-    await clearNotifPromise;
+    await clearNotifPromise; // make sure the notifications are cleared
+
     return parsedNotifs;
 }
 
 module.exports = {
-    getBrandNewNotifications,
-    clearNewNotifications,
     getAndParseNewNotifications
 };
