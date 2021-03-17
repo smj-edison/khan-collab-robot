6d78adc5a29966255b11d6639367799817cd003f
smj-edison <stephen.mason.jones@gmail.com>
2021-03-16 21:41:39 -0700

    Cascade delete spinoffs for deleteprogram command

--- src/commands/deleteprogram.js
+++ src/commands/deleteprogram.js
@@ -1,4 +1,4 @@
-const {deleteProgram} = require("ka-api").programs;
+const {getSpinoffs, deleteProgram} = require("ka-api").programs;
 const {getProgramCodeAndHeaders} = require("../metadata/programs");
 const {isAuthor} = require("../authorization/authorization.js");
 
@@ -12,10 +12,17 @@ async function deleteprogram(cookies, args, kaid) {
         return "You are not authorized to do this.";
     }
 
+    // get all the history program's spinoffs (by bors)
+    const codeRevisionProgramsToDelete = (await getSpinoffs(codeHeaders.historyprogramid)).scratchpads.filter(spinoff => {
+        return spinoff.authorKaid === process.env.BOT_KAID;
+    }).map(spinoff => spinoff.url.match(/\d+(?=\?)?/)[0]);
+
+    const revisionsDeletePromises = codeRevisionProgramsToDelete.map(codeRevisionProgram => deleteProgram(cookies, codeRevisionProgram));
+
     await Promise.all([
         deleteProgram(cookies, programId),
         deleteProgram(cookies, codeHeaders.historyprogramid)
-    ]);
+    ].concat(revisionsDeletePromises));
 
     return "Successfully deleted program.";
 }
